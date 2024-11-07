e4c08af5e08538cb7e62ca3263843e19e968c11c
Moussa Elrahmoun <672637@stud.hvl.no>
2024-11-07 20:49:28 +0100

    1
    1

--- Wine_Quality/WineQualityApp.py
+++ Wine_Quality/WineQualityApp.py
@@ -2,8 +2,7 @@ import pickle
 import pandas as pd
 import streamlit as st
 #upload  the data 
-DataModelleing =pickle.load(open(r'/Users/moussaelrahmoun/Desktop/ML_Project/Wine_Quality/WienQuality_prediction.sav','rb'))
-st.title('wine_qualti')
+DataModelleing = pickle.load(open("WienQuality_prediction.sav", "rb"))
 st.sidebar.header('feature selection ')
 fixed_acidity = st.number_input("Fixed Acidity")
 volatile_acidity = st.number_input("Volatile Acidity")
